627faa8bbe5d3ebf7b5f507c647a59961a8b7f56
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-05-04 11:35:50 -0300

    Initial commit

---
+++ src/br/mathrandom/MathRandom.java
@@ -0,0 +1,13 @@
+package br.mathrandom;
+
+    public class MathRandom {
+               
+    public static void main(String[] args) {
+    
+
+        float ale = (float) (1 + Math.random() * (5-1));
+   
+        System.out.printf("%1.1f",ale);
+
+    }
+}
